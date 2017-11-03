7c2a67f733f9dce5cc8cd02f0d4be2126ecf448b
Hagar Shilo <strayblues@gmail.com>
2017-11-03 16:42:34 +0200

    Add jQuery and incorporate code from another project

--- buyoosh.js
+++ buyoosh.js
@@ -1,3 +1,4 @@
+/*
 var shoppingList = [];
 
 function getNewItem()
@@ -42,3 +43,30 @@ function addShoppingListItem(newItem)
 
     shoppingList.push(newItem);
 }
+*/
+// Check off specific todos by clicking
+$('ul').on('click', 'li', function(){
+ $(this).toggleClass('completed');
+});
+
+// Click on X to delete todos
+$('ul').on('click', 'span', function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    // Grab new todo text from input
+    var todoText = $(this).val();
+    $(this).val('');
+    // Create a new li and add to ul
+    $('ul').append('<li> <span><i class="fa fa-trash"></i></span> ' + todoText + '</li>')
+  }
+});
+
+$('.fa-plus').click(function(){
+  $("input[type='text']").fadeToggle();
+});
