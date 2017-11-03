ae21a00a5fca87d1f66be15bb2211f2b51935b45
Hagar Shilo <strayblues@gmail.com>
2017-11-03 22:19:07 +0200

    Add Bootstrap

--- buyoosh.js
+++ buyoosh.js
@@ -63,7 +63,7 @@ $("input[type='text']").keypress(function(event){
     var todoText = $(this).val();
     $(this).val('');
     // Create a new li and add to ul
-    $('ul').append('<li> <span><i class="fa fa-trash"></i></span> ' + todoText + '</li>')
+    $('ul').append('<li class="list-group-item"> <span><i class="fa fa-trash"></i></span> ' + todoText + '</li>')
   }
 });
 
